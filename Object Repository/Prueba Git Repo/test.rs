6832ad4ae9480d59389b542a016e31b13281cb2a
alopez <alopez@SARDT2072.sar.gruposoftland.com>
2024-03-05 12:57:55 -0300

    prueba con archivo

---
+++ Object Repository/Prueba Git Repo/test.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>test</name>
+   <tag></tag>
+   <elementGuidId>4a21faaa-f97a-48bc-b864-a6a0a69c1acf</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>dasdasdasd</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
